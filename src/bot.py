2758111d668e93658c309a32f6992a79a379f4a1
Zukunftsmusik <5472896+Zukunftsmusik@users.noreply.github.com>
2021-10-08 11:27:32 +0200

    Added command invite

--- src/bot.py
+++ src/bot.py
@@ -4,22 +4,46 @@ from discord.ext import commands
 import app_settings
 
 
-BOT = commands.Bot(command_prefix='vv ')
 
+
+
+BOT = commands.Bot(
+    command_prefix='vv ',
+    intents=discord.Intents.all()
+)
+
+
+
+
+
+# ---------- Basic role management ----------
 
 @BOT.group(name='role', invoke_without_command=True)
 async def cmd_role(ctx: commands.Context) -> None:
     pass
 
 
-@cmd_role.command(name='add')
+@cmd_role.command(name='add', brief='Add a role to the specified members')
 async def cmd_role_add(ctx: commands.Context, role: discord.Role, *, user_ids: str) -> None:
     pass
 
 
-@cmd_role.command(name='clear')
+@cmd_role.command(name='clear', brief='Remove a role from all members')
 async def cmd_role_clear(ctx: commands.Context, role: discord.Role) -> None:
     pass
+
+
+
+
+
+# ---------- Bot management ----------
+
+@BOT.command(name='invite', brief='Produce invite link')
+async def cmd_invite(ctx: commands.Context) -> None:
+    ctx.send('https://discordapp.com/oauth/authorize?scope=138982837312&client_id=895959886834331658')
+
+
+
 
 
 if __name__ == '__main__':
