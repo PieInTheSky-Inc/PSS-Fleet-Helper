3577fb667a6422718af933d91e1ba21da1efd100
Zukunftsmusik <5472896+Zukunftsmusik@users.noreply.github.com>
2021-10-27 22:07:36 +0200

    Refactoring

--- src/main.py
+++ src/main.py
@@ -1,24 +1,19 @@
 import asyncio
-from typing import Callable as _Callable
-from typing import List as _List
-from typing import Optional as _Optional
-from typing import Tuple as _Tuple
 
 import discord
-from discord.ext import commands
-from discord.ext.commands import Context
-from discord.ext.commands.core import bot_has_guild_permissions, is_owner
+from discord.ext.commands import Bot as _Bot
+from discord.ext.commands import Context as _Context
+from discord.ext.commands.errors import CommandInvokeError as _CommandInvokeError
+from discord.ext.commands import when_mentioned_or as _when_mentioned_or
 
 import app_settings
-from confirmator import Confirmator
 import model
-from model import utils
 
 
 # ---------- Setup ----------
 
-BOT = commands.Bot(
-    commands.when_mentioned_or('vivi '),
+BOT = _Bot(
+    _when_mentioned_or('vivi '),
     intents=discord.Intents.all(),
     activity=discord.activity.Activity(type=discord.ActivityType.playing, name='vivi help')
 )
@@ -28,11 +23,13 @@ BOT = commands.Bot(
 # ---------- Event handlers ----------
 
 @BOT.event
-async def on_command_error(ctx: Context, err: Exception) -> None:
+async def on_command_error(ctx: _Context,
+                            err: Exception
+                        ) -> None:
     if app_settings.THROW_COMMAND_ERRORS:
         raise err
 
-    if isinstance(err, commands.errors.CommandInvokeError):
+    if isinstance(err, _CommandInvokeError):
         err = err.original
     error_type = type(err).__name__
     error_text = (err.args[0] or '') if err.args else ''
