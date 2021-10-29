f1fc8f0edcc32bd0cb52914e0ac3548f9c4f6a15
Zukunftsmusik <5472896+Zukunftsmusik@users.noreply.github.com>
2021-10-29 14:11:39 +0200

    Updated version

--- src/app_settings.py
+++ src/app_settings.py
@@ -11,4 +11,4 @@ DISCORD_BOT_TOKEN: str = _os.environ.get('VIVIBOT_DISCORD_BOT_TOKEN')
 THROW_COMMAND_ERRORS: bool = bool(int(_os.environ.get('THROW_COMMAND_ERRORS', 0)))
 
 
-VERSION: str = '0.3.0'
+VERSION: str = '0.3.1'
