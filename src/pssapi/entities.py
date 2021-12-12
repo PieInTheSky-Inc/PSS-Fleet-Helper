fe4581f8b6a4549d553febe248495ebbce715221
Zukunftsmusik <5472896+Zukunftsmusik@users.noreply.github.com>
2021-12-12 22:26:04 +0100

    wip

--- src/pssapi/entities.py
+++ src/pssapi/entities.py
@@ -13,10 +13,10 @@ class PssEntityBase():
     pass
 
 
-class PssAlliance(PssEntityBase):
+class PssAllianceRaw(PssEntityBase):
     def __init__(self, alliance_info: _Dict[str, _Any]) -> None:
         self.__alliance_info: _Dict[str, _Any] = dict(alliance_info)
-        self.__members: _List['PssUser'] = None
+        self.__members: _List['PssUserRaw'] = None
         self.__members_updated: _datetime = None
 
 
@@ -37,7 +37,7 @@ class PssAlliance(PssEntityBase):
         return _convert.str_to_int(self.__alliance_info.get('AllianceId'))
 
     @property
-    def members(self) -> _Optional[_List['PssUser']]:
+    def members(self) -> _Optional[_List['PssUserRaw']]:
         return self.__members
 
     @property
@@ -61,11 +61,11 @@ class PssAlliance(PssEntityBase):
         return _convert.str_to_int(self.__alliance_info.get('Trophy'))
 
 
-    async def fetch_members(self, access_token: str) -> _List['PssUser']:
+    async def fetch_members(self, access_token: str) -> _List['PssUserRaw']:
         from . import alliance_service as _alliance_service
-        members: _List['PssUser'] = await _alliance_service.list_users(self.id, access_token)
-        self.__members = members
+        members: _List['PssUserRaw'] = await _alliance_service.list_users(self.id, access_token)
         self.__members_updated = _utils.get_utc_now()
+        self.__members = members
         for member in members:
             if member.alliance_id == self.id:
                 member.set_alliance(self)
@@ -75,21 +75,42 @@ class PssAlliance(PssEntityBase):
 
 
 
-class PssUser(PssEntityBase):
-    def __init__(self, user_info: _Dict[str, _Any]) -> None:
-        self.__user_info = dict(user_info)
-        alliance_info = self.__user_info.get('Alliance')
-        if alliance_info:
-            self.set_alliance(PssAlliance(alliance_info))
+class PssAlliance(PssAllianceRaw):
+    def __init__(self, alliance_info: _Dict[str, _Any]):
+        super().__init__(alliance_info)
 
 
     @property
-    def alliance(self) -> 'PssAlliance':
-        return self.__user_info['Alliance']
+    def members(self) -> _Optional[_List['PssUser']]:
+        return self.__members
+
+
+    async def fetch_members(self, access_token: str) -> _List['PssUser']:
+        from . import alliance_service as _alliance_service
+        members: _List['PssUserRaw'] = await _alliance_service.list_users(self.id, access_token)
+        super().__members_updated = _utils.get_utc_now()
+        super().__members = [PssUser(member.user_info) for member in members]
+        for member in members:
+            if member.alliance_id == self.id:
+                member.set_alliance(self)
+        return members
+
+
+
+
+
+class PssUserRaw(PssEntityBase):
+    def __init__(self, user_info: _Dict[str, _Any]) -> None:
+        self.__user_info: _Dict[str, _Any] = dict(user_info)
+
 
     @property
     def alliance_id(self) -> _Optional[int]:
         return _convert.str_to_int(self.__user_info.get('AllianceId'))
+
+    @property
+    def alliance_info(self) -> _Dict[str, _Any]:
+        return self.__user_info.get('Alliance')
 
     @property
     def alliance_join_date(self) -> _Optional[_datetime]:
@@ -178,6 +199,30 @@ class PssUser(PssEntityBase):
     @property
     def trophy(self) -> _Optional[int]:
         return _convert.str_to_int(self.__user_info.get('Trophy'))
+
+    @property
+    def user_info(self) -> _Dict[str, _Any]:
+        return self.__user_info
+
+
+    def set_alliance(self, alliance: 'PssAllianceRaw') -> None:
+        self.__user_info['Alliance'] = alliance
+        self.__user_info['AllianceId'] = alliance.id
+
+
+
+
+
+class PssUser(PssUserRaw):
+    def __init__(self, user_info: _Dict[str, _Any]):
+        super().__init__(user_info)
+        if 'Alliance' in self.__user_info.keys():
+            self.set_alliance(PssAlliance(alliance_info))
+
+
+    @property
+    def alliance(self) -> 'PssAlliance':
+        return self.__user_info['Alliance']
 
 
     def set_alliance(self, alliance: 'PssAlliance') -> None:
