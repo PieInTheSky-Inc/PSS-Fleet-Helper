bedc8ba24e0316c578f5c6ebaf2db049f2bcfaa5
Zukunftsmusik <5472896+Zukunftsmusik@users.noreply.github.com>
2021-12-12 22:26:00 +0100

    wip

--- src/pssapi/__init__.py
+++ src/pssapi/__init__.py
@@ -1,6 +1,6 @@
 
 
-from .entities import PssAlliance, PssEntityBase, PssUser
+from .entities import PssAllianceRaw, PssEntityBase, PssUserRaw
 from .alliance_service import list_users, search_alliances
 from .login import login
 from . import types
