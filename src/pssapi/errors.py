0685fd3592a74d94678608ba60b1f9449c26cfd4
Zukunftsmusik <5472896+Zukunftsmusik@users.noreply.github.com>
2022-05-04 09:57:45 +0200

    Fixed bug in error handling

--- src/pssapi/errors.py
+++ src/pssapi/errors.py
@@ -4,29 +4,34 @@ from xml.etree.ElementTree import ParseError as _XmlParseError
 
 
 class PssApiError(Exception):
+    def __init__(self, message: str):
+        self.message = message
+
+
     def __str__(self) -> str:
         msg = [
             type(self).__name__,
             self.message
         ]
+        return '\n'.join(msg)
+
 
     def __repr__(self) -> str:
         return self.__str__()
+
 
 
 class ServerMaintenanceError(PssApiError):
     pass
 
 
+
 class PssXmlError(PssApiError, _XmlParseError):
     def __init__(self, raw_xml: str, parse_error: _XmlParseError):
-        self.__raw_xml: str = raw_xml
-        self.__parse_error: _XmlParseError = parse_error
-
-    @property
-    def raw_xml(self) -> str:
-        return self.__raw_xml
+        super().__init__(parse_error.msg)
+        self.raw_xml: str = raw_xml
+        self.parse_error: _XmlParseError = parse_error
 
     @property
     def position(self) -> _Tuple[int, int]:
-        return self.__parse_error.position
+        return self.parse_error.position
