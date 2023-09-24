fb56bb311083ebd74a2a1bfe2128c8d003265c2f
The worst <5472896+Zukunftsmusik@users.noreply.github.com>
2023-09-24 18:14:42 +0200

    Add Fleet entity

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -2,7 +2,22 @@ from . import database
 from . import errors
 from . import model_settings
 from . import orm
+from .fleet import Fleet
 from .setup import setup as setup_model
 from .reaction_role import ReactionRole, ReactionRoleChange, ReactionRoleRequirement
 from .chat_log import PssChatLogger
 from src.model.pssapi_discord_bot import PssApiDiscordBot
+
+__all__ = [
+    database.__name__,
+    errors.__name__,
+    model_settings.__name__,
+    orm.__name__,
+    setup_model.__name__,
+    Fleet.__name__,
+    PssApiDiscordBot.__name__,
+    PssChatLogger.__name__,
+    ReactionRole.__name__,
+    ReactionRoleChange.__name__,
+    ReactionRoleRequirement.__name__,
+]
